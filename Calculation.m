fad978f9c07b2e8c584f241ffd3767ffab34279f
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2024-06-16 21:45:16 +0500

    Add files via upload

--- Calculation.m
+++ Calculation.m
@@ -1,5 +1,11 @@
-%Функция расчёта режима. Не реализована заданная точность расчёта уст.
-%режима, исходная привязка напряжений V к Unom, СХН
+%Функция расчёта режима
+for i=1:size(model.BusTable,1)
+    if model.BusTable(i).Vinst~=0
+        model.BusTable(i).V=model.BusTable(i).Vinst;
+    else
+    model.BusTable(i).V=model.BusTable(i).Unom
+    end
+end
 Y = zeros(size(model.BusTable,1));
 Y=CreateYTable(model,Y);
 model=Iteration(model,Y); 
--- Iteration.m
+++ Iteration.m
@@ -1,8 +1,37 @@
 
 %Одна итерация методом Ньютона
 function model1 = Iteration(model,Y)
+   discrepancy=CalculateDisperancy(model,Y);
+   jacobian=CalculateJacobian(model,Y); 
+        %Вектор изменения
+    dx(:,1)=inv(jacobian)*(-discrepancy(:,1));
+    dx(:,2)=discrepancy(:,2);
+    dx(:,3)=discrepancy(:,3);
+   %Присвоение новых значений переменных
+   for i=1:size(dx,1)
+        if dx(i,3)==1 % соответсвует невязке по реактивной мощности 
+            model.BusTable(dx(i,2)).V=model.BusTable(dx(i,2)).V+dx(i,1); 
+        end
+        if dx(i,3)==0 % соответсвует невяке по активной мощности
+            model.BusTable(dx(i,2)).D=model.BusTable(dx(i,2)).D+dx(i,1);
+        end        
+   end
+  model1=model;
+  maxDisperancy= 0;
+  for i=1:size(discrepancy,1)
+      if discrepancy(i,1) > maxDisperancy && discrepancy(i,3)==0;
+          maxDisperancy=discrepancy(i,1);
+      end
+  end
+  if maxDisperancy>0.1
+      model1=Iteration(model1,Y);
+  end
 
-    %Вектор невязок
+end
+
+
+function disperancy1 = CalculateDisperancy (model,Y)
+ %Вектор невязок
     for i=1:size(model.BusTable,1)
        discrepancy(i,1)=0;
        discrepancy(i,2)=i; %Пометка номера узла
@@ -16,14 +45,12 @@ function model1 = Iteration(model,Y)
            discrepancy(i,1)=discrepancy(i,1)+imag(Y(i,j))*model.BusTable(i).V*model.BusTable(j).V*cos(model.BusTable(i).D-model.BusTable(j).D)-real(Y(i,j))*model.BusTable(i).V*model.BusTable(j).V*sin(model.BusTable(i).D-model.BusTable(j).D)
            end              
            end       
-           discrepancy(i+size(model.BusTable,1),1)=discrepancy(i+size(model.BusTable,1),1)-model.BusTable(i).Pload+model.BusTable(i).Pgen;
+           discrepancy(i+size(model.BusTable,1),1)=discrepancy(i+size(model.BusTable,1),1)-model.BusTable(i).Pload*CalculatePSLC(model,i)+model.BusTable(i).Pgen;
            if model.BusTable(i).Type==3 
-           discrepancy(i,1)=discrepancy(i,1)+model.BusTable(i).Qload;
+           discrepancy(i,1)=discrepancy(i,1)+model.BusTable(i).Qload*CalculateQSLC(model,i);
            end 
     end
-    Y1=Y;
-
-    %Исключение базовых и генераторных узлов в веторе невязок
+    %Исключение базовых и генераторных узлов в векторе невязок
     x=1; deleted=0; i=1;  
     while (x<=size(model.BusTable,1)-deleted)
         if model.BusTable(i).Type==4
@@ -40,8 +67,10 @@ function model1 = Iteration(model,Y)
         x=x+1;
         i=i+1;
     end
-    
+    disperancy1=discrepancy;
+end
 
+function jacobian1 = CalculateJacobian (model,Y)
     % Диагональные элементы матрицы Якоби
     for i=1:size(model.BusTable,1)
         jacobian(i,i+size(model.BusTable,1))=0;
@@ -63,7 +92,6 @@ function model1 = Iteration(model,Y)
             end
         end          
     end
-
     % Недиагональные элементы матрицы Якоби
     for i=1:size(model.BusTable,1)
         for j=1:size(model.BusTable,1)
@@ -77,7 +105,6 @@ function model1 = Iteration(model,Y)
         end
         
     end
-
     %Исключение базового и генерирующего узла в матрице Якоби
     x=1; deleted=0; i=1;
     while (x<=size(model.BusTable,1)-deleted)
@@ -98,20 +125,30 @@ function model1 = Iteration(model,Y)
         x=x+1;
         i=i+1;
     end
-     
-    %Вектор изменения
-    dx(:,1)=inv(jacobian)*(-discrepancy(:,1));
-    dx(:,2)=discrepancy(:,2);
-    dx(:,3)=discrepancy(:,3);
+    jacobian1=jacobian;
 
-    %Присвоение новых значений переменных
-   for i=1:size(dx,1)
-        if dx(i,3)==1
-            model.BusTable(dx(i,2)).V=model.BusTable(dx(i,2)).V+dx(i,1);
+end
+%Расчёт коэф. для СХН по активной мощности
+function kpslc = CalculatePSLC(model,i)
+    if  model.BusTable(i).Plslc~=0;
+        k=model.BusTable(i).V/model.BusTable(i).Unom;       
+        kpslc=0.83-0.3*k+0.47*k^2;       
+    else
+        kpslc=1;
+    end
+end
+%Расчёт коэф. для СХН по реактивной мощности
+function kqslc = CalculateQSLC(model,i)
+    if model.BusTable(i).Type==3 && model.BusTable(i).Plslc~=0;
+        k=model.BusTable(i).V/model.BusTable(i).Unom;
+        if k < 0.815 
+            kqslc=0.721+0.158*k;
+        elseif k >1.2 
+            kqslc=1.49;
+        else
+            kqslc=3.7-7*k+4.3*k^2;
         end
-        if dx(i,3)==0
-            model.BusTable(dx(i,2)).D=model.BusTable(dx(i,2)).D+dx(i,1);
-        end        
-   end
-  model1=model;
+    else
+        kqslc=1;
+    end
 end
